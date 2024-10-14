82fe52f99c3feda0c934745de80f9d9093d3af7a
Eitan-Oz <bachareitanoz@gmail.com>
2024-10-14 12:47:59 +0300

    italy, france, belgium works in the combobox

--- projact in coding/MainWindow.xaml.cs
+++ projact in coding/MainWindow.xaml.cs
@@ -27,44 +27,97 @@ namespace projact_in_coding
 
         private void italyBtn_Click(object sender, RoutedEventArgs e)
         {
-            this.rec1.Fill = new SolidColorBrush(Colors.Green);
-            this.rec2.Fill = new SolidColorBrush(Colors.White);
-            this.rec3.Fill = new SolidColorBrush(Colors.Red);
+            this.Col1.Fill = new SolidColorBrush(Colors.Green);
+            this.Col2.Fill = new SolidColorBrush(Colors.White);
+            this.Col3.Fill = new SolidColorBrush(Colors.Red);
+        }
+
+        private void italy_Click()
+        {
+            this.Col1.Fill = new SolidColorBrush(Colors.Green);
+            this.Col2.Fill = new SolidColorBrush(Colors.White);
+            this.Col3.Fill = new SolidColorBrush(Colors.Red);
         }
 
         private void franceBtn_Click(object sender, RoutedEventArgs e)
         {
-            this.rec1.Fill = new SolidColorBrush(Colors.Blue);
-            this.rec2.Fill = new SolidColorBrush(Colors.White);
-            this.rec3.Fill = new SolidColorBrush(Colors.Red);
+            this.Col1.Fill = new SolidColorBrush(Colors.Blue);
+            this.Col2.Fill = new SolidColorBrush(Colors.White);
+            this.Col3.Fill = new SolidColorBrush(Colors.Red);
+
+        }
+
+        private void france_Click()
+        {
+            this.Col1.Fill = new SolidColorBrush(Colors.Blue);
+            this.Col2.Fill = new SolidColorBrush(Colors.White);
+            this.Col3.Fill = new SolidColorBrush(Colors.Red);
 
         }
 
         private void belgiumBtn_Click(object sender, RoutedEventArgs e)
         {
-            this.rec1.Fill = new SolidColorBrush(Colors.Black);
-            this.rec2.Fill = new SolidColorBrush(Colors.Yellow);
-            this.rec3.Fill = new SolidColorBrush(Colors.Red);
+            this.Col1.Fill = new SolidColorBrush(Colors.Black);
+            this.Col2.Fill = new SolidColorBrush(Colors.Yellow);
+            this.Col3.Fill = new SolidColorBrush(Colors.Red);
+
+        }
+
+        private void belgium_Click()
+        {
+            this.Col1.Fill = new SolidColorBrush(Colors.Black);
+            this.Col2.Fill = new SolidColorBrush(Colors.Yellow);
+            this.Col3.Fill = new SolidColorBrush(Colors.Red);
+
+        }
+
+        private void horizontalFlag()
+        {
+            this.HorizontalBox.Visibility = Visibility.Collapsed;
+            this.VerticalBox.Visibility = Visibility.Visible;
 
         }
 
         private void contrycbox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-           switch (this.Content.ToString().ToLower() )
+            ComboBoxItem selectedItem = this.contrycbox.SelectedItem as ComboBoxItem;
+            //belgium_Click();
+           switch (selectedItem.Content.ToString().ToLower() )
             {
                 case "italy":
-                    this.rec1.Fill = new SolidColorBrush(Colors.Green);
-                    this.rec2.Fill = new SolidColorBrush(Colors.White);
-                    this.rec3.Fill = new SolidColorBrush(Colors.Red);
-
+                     
+                        horizontalFlag();
+                        italy_Click();
+                    
                     break;
-
+                    
 
                 case "france":
-                    franceBtn_Click(sender, e);
+                    horizontalFlag();
+                    france_Click();
+
+                    break;
+                case "belgium":
+                    horizontalFlag();
+                    belgium_Click();
+
+                    break;
+                case "germany":
+
 
 
                     break;
+                case "austria":
+
+
+
+                    break;
+                case "bolivia":
+
+
+
+                    break;
+
             }
         }
     }
